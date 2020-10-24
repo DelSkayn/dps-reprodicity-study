1188e511596396e97ad4d85b259ac9fc4d6d2163
Mees Delzenne <mees.delzenne@gmail.com>
2020-10-24 21:59:32 +0200

    Old version took more then a day to run, weirdly slow, trying new method

--- graph_converter_2/src/main/java/GraphConverter.java
+++ graph_converter_2/src/main/java/GraphConverter.java
@@ -1,33 +1,74 @@
+
+/*
+ * Copyright (C) 2003-2014 Paolo Boldi and Sebastiano Vigna
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+import it.unimi.dsi.fastutil.ints.IntArrayFIFOQueue;
 import it.unimi.dsi.webgraph.ImmutableGraph;
-import it.unimi.dsi.webgraph.NodeIterator;
+import it.unimi.dsi.webgraph.LazyIntIterator;
 
-import java.lang.reflect.InvocationTargetException;
-import java.io.*;
+import java.io.IOException;
+import java.util.Arrays;
 
 
-public class GraphConverter {
-    public static void main(String[] arg) throws ClassCastException, IllegalArgumentException, SecurityException, IllegalArgumentException, IOException {
-        if ( arg.length != 1 ) {
-            System.err.println( "Usage: BV2Ascii BASENAME" );
-            return;
-        }
+/** The main method of this class loads an arbitrary {@link it.unimi.dsi.webgraph.ImmutableGraph}
+ * and performs a breadth-first visit of the graph (optionally starting just from a given node, if provided,
+ * in which case it prints the eccentricity of the node, i.e., the maximum distance from the node).
+ */
 
-        final ImmutableGraph graph = it.unimi.dsi.webgraph.ImmutableGraph.loadOffline( arg[0] );
+public class GraphConverter{
 
-        NodeIterator nodeIterator = graph.nodeIterator();
-        int curr, d;
-        int[] suc;
-        FileOutputStream file = new FileOutputStream(arg[0] + ".net");
-        BufferedOutputStream outStreamB = new BufferedOutputStream(file, 4096);
-        PrintStream outStream = new PrintStream( outStreamB );
 
-        while( nodeIterator.hasNext() ) {
-            curr = nodeIterator.nextInt();
-            d = nodeIterator.outdegree();
-            suc = nodeIterator.successorArray();
+    static public void main(String[] arg) throws IllegalArgumentException, SecurityException,  IOException {
 
-            for( int j=0; j<d; j++ ) {
-                outStream.println(curr + "\t " + (suc[j]) );
+        final String basename = arg[0];
+        ImmutableGraph graph = ImmutableGraph.load( basename);
+
+        final IntArrayFIFOQueue queue = new IntArrayFIFOQueue();
+        final int n = graph.numNodes();
+        final int[] dist = new int[ n ];
+
+        Arrays.fill( dist, Integer.MAX_VALUE ); // Initially, all distances are infinity.
+
+        int curr, succ, ecc = 0;
+
+
+        for(int i = 0; i < n; i++ ) {
+            if ( dist[ i ] == Integer.MAX_VALUE ) { // Not already visited
+                queue.enqueue( i );
+                dist[ i ] = 0;
+
+                LazyIntIterator successors;
+
+                while( ! queue.isEmpty() ) {
+                    curr = queue.dequeueInt();
+                    successors = graph.successors( curr );
+                    int d = graph.outdegree( curr );
+                    while( d-- != 0 ) {
+                        succ = successors.nextInt();
+                        if ( dist[ succ ] == Integer.MAX_VALUE ) {
+                            dist[ succ ] = dist[ curr ] + 1;
+                            ecc = Math.max( ecc, dist[ succ ] );
+                            queue.enqueue( succ );
+                            System.out.println( i + " \t " + succ + " ");
+                        }
+                    }
+                }
             }
         }
     }
